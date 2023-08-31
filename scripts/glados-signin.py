dba7cdcd60f11ce4718089c892fc3a39c4b46092
hhhhhzhen <suhouzhen2020@gmail.com>
2023-08-31 18:54:12 +0800

    Update glados-signin.py

--- scripts/glados-signin.py
+++ scripts/glados-signin.py
@@ -15,9 +15,7 @@ url = "https://glados.rocks/api/user/checkin"
 cookies = {
     "koa:sess": "eyJ1c2VySWQiOjM4Mzg4NSwiX2V4cGlyZSI6MTcxOTM5NzgwNTg2NywiX21heEFnZSI6MjU5MjAwMDAwMDB9",
     "koa:sess.sig": "yIGXjzGJaGkG06usfNfX07Tg60A",
-    "__stripe_mid": "66fe41cb-0264-4356-91c1-e066e104abbe25967c",
-    "_ga_CZFVKMNT9J": "GS1.1.1690512957.9.1.1690513362.0.0.0",
-    "_ga": "GA1.1.1156446483.1676172823"
+    "__stripe_mid": "66fe41cb-0264-4356-91c1-e066e104abbe25967c"
 }
 # from 请求负载
 value = {"token": "glados.one"}
@@ -40,6 +38,5 @@ msg = {
 
 #print(msg)
 
-# password 是 qq 的密码框(需要开启服务)
-yag = yagmail.SMTP(user=email, password=password, host='smtp.qq.com')
+yag = yagmail.SMTP(user=email, password=password, host='smtp.163.com')
 yag.send(to=email, subject='glados sign in', contents=msg)
